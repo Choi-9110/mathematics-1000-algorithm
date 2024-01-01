a9833efae8561fc4dea893cfbdf7cede3564624c
ChoiHK <gusrms2346@naver.com>
2024-01-01 22:47:34 +0900

    Feat : 1002 곱셈 까지 끝, cmd 용어를 조금더 익숙해지기위해 하루에 몇개씩 사용하고 코멘트를 남긴다.

---
+++ math-1002.js
@@ -0,0 +1,26 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function prompt(question) {
+  return new Promise(resolve => rl.question(question, resolve));
+}
+
+async function main() {
+  console.log("입력받는 두개의 정수를 곱한다. ");
+
+  const A = parseInt(await prompt('A의 값 입력: '), 10);
+  const B = parseInt(await prompt('B의 값 입력: '), 10);
+
+  const sum = A * B;
+
+  console.log(`A * B = ${sum}`);
+
+  rl.close();
+}
+
+main();
+
