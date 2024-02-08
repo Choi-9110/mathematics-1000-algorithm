c9bcae12853e9e86db83f0013dfb79db0d92a2e6
Wish-poll <wishpoll.info@gmail.com>
2024-02-08 13:33:59 +0900

    Faet :사칙연산 소수점 단위는 일반적으로 0으로 설정

--- math-1004.js
+++ math-1004.js
@@ -15,19 +15,24 @@ async function main() {
   const A = parseInt(await prompt('A의 값 입력: '), 10);
   const B = parseInt(await prompt('B의 값 입력: '), 10);
 
-  const sum = A / B;
+  const resultSum = A + B;
+  const resultDif = A - B;
+  const resultProd = A * B;
+  const resultQuot = A / B;
+  const resultRem = A % B;
 
-   
-   // 소수점 자르기  .toFixed(2) 
-   // 소수점 2자리까지 노출 이후 나머지는 자른다. 
 
-   // 전체 자릿수에서 자른다 
-   // toPrecision(2)    123.456 ==>  1.2e+2
-   console.log(`A + B = ${sum.toFixed(9)}`);
-   console.log(`A - B = ${sum.toFixed(9)}`);
-   console.log(`A * B = ${sum.toFixed(9)}`);
-   console.log(`A / B = ${sum.toFixed(9)}`);
-   console.log(`A % B = ${sum.toFixed(9)}`);
+
+  // 소수점 자르기  .toFixed(2) 
+  // 소수점 2자리까지 노출 이후 나머지는 자른다. 
+
+  // 전체 자릿수에서 자른다 
+  // toPrecision(2)    123.456 ==>  1.2e+2
+  console.log(`A + B = ${resultSum.toFixed(0)}`);
+  console.log(`A - B = ${resultDif.toFixed(0)}`);
+  console.log(`A * B = ${resultProd.toFixed(0)}`);
+  console.log(`A / B = ${resultQuot.toFixed(3)}`);
+  console.log(`A % B = ${resultRem.toFixed(0)}`);
 
   rl.close();
 }
